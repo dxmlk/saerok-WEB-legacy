056ea91e84d282b6d622d29b1439fd8c76010a81
dxmlk <1101sky@korea.ac.kr>
2025-11-17 17:54:40 +0900

    feat: 라우팅 페이지 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,10 @@
-// App.tsx
-import { useEffect } from "react";
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
-import { Mobile, PC } from "./components/Responsive";
-import MobilePage from "./components/tempPages/MobilePage";
-import PCPage from "./components/tempPages/PCPage";
-
-const APP_STORE_URL =
-  "https://apps.apple.com/kr/app/%EC%83%88%EB%A1%9D-%EC%9D%BC%EC%83%81-%EC%86%8D%EC%9D%98-%ED%83%90%EC%A1%B0-%EC%9D%BC%EC%A7%80/id6744866662"; // ← 여기에 실제 앱스토어 주소 넣으세요
+import { createRouter } from "./routes";
 
 function App() {
-  useEffect(() => {
-    const ua = navigator.userAgent || navigator.vendor || (window as any).opera;
-
-    // iPhone / iPad / iPod 체크
-    const isIOS =
-      /iPhone|iPad|iPod/i.test(ua) ||
-      // iPadOS 13+가 desktop Safari처럼 나오는 경우 처리
-      (navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1);
-
-    if (isIOS) {
-      window.location.href = APP_STORE_URL;
-    }
-  }, []);
-
-  // const router = createRouter();
-  // return <RouterProvider router={router} />;
-
-  return (
-    <>
-      <Mobile>
-        <MobilePage />
-      </Mobile>
-      <PC>
-        <PCPage />
-      </PC>
-    </>
-  );
+  const router = createRouter();
+  return <RouterProvider router={router} />;
 }
 
 export default App;
